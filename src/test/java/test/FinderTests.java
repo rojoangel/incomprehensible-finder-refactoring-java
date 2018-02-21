48e1366c2d3357ae245ad0cd19729f01494ad33a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-21 20:37:44 +0100

    move sample data to a separate class - to enable reuse across different test classes

--- src/test/java/test/FinderTests.java
+++ src/test/java/test/FinderTests.java
@@ -1,23 +1,18 @@
 package test;
-import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.junit.Test;
-
-import algorithm.Pair;
 import algorithm.FT;
 import algorithm.Finder;
+import algorithm.Pair;
 import algorithm.Person;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static test.SamplePerson.*;
 
 public class FinderTests {
-
-	private Person sue = new Person("Sue", new Date(50, 0, 1));
-	private Person greg = new Person("Greg", new Date(52, 5, 1));
-	private Person sarah = new Person("Sarah", new Date(82, 0, 1));
-	private Person mike = new Person("Mike", new Date(79, 0, 1));
 
 	@Test
 	public void Returns_Empty_Results_When_Given_Empty_List() {
---
+++ src/test/java/test/SamplePerson.java
@@ -0,0 +1,12 @@
+package test;
+
+import algorithm.Person;
+
+import java.util.Date;
+
+public class SamplePerson {
+    public static Person sue = new Person("Sue", new Date(50, 0, 1));
+    public static Person greg = new Person("Greg", new Date(52, 5, 1));
+    public static Person sarah = new Person("Sarah", new Date(82, 0, 1));
+    public static Person mike = new Person("Mike", new Date(79, 0, 1));
+}
