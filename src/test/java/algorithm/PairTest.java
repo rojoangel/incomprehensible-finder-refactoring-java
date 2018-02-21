94a72a1e117c0d6305e03d9ce7839cf60f45c17e
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-21 21:01:24 +0100

    unit test pair

---
+++ src/test/java/algorithm/PairTest.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static test.SamplePerson.greg;
+import static test.SamplePerson.sue;
+
+public class PairTest {
+
+    @Test
+    public void a_pair_of_two_people_should_always_be_the_same() {
+        Pair onePair = new Pair(sue, greg);
+        Pair reversedPair = new Pair(greg, sue);
+        assertEquals(onePair, reversedPair);
+    }
+
+    @Test
+    public void birthDateDistance_should_be_the_difference_between_dates() {
+        Pair onePair = new Pair(sue, greg);
+        assertEquals(greg.getBirthDate().getTime() - sue.getBirthDate().getTime(), onePair.getBirthDateDistance());
+    }
+}
