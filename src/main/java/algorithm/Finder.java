708c09b1730274cc212e7ba0083c1dbd7b70269d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-21 23:36:01 +0100

    get rid of personList

--- src/main/java/algorithm/Finder.java
+++ src/main/java/algorithm/Finder.java
@@ -1,17 +1,15 @@
 package algorithm;
+
 import java.util.List;
 
 public class Finder {
-	private final List<Person> personList;
-	private final People people;
+    private final People people;
 
     public Finder(List<Person> peopleList) {
-		this.personList = peopleList;
-		this.people = new People(peopleList);
-	}
+        this.people = new People(peopleList);
+    }
 
-	public Pair Find(Algorithm algorithm) {
-
+    public Pair Find(Algorithm algorithm) {
         return people.pairs().find(algorithm);
     }
 }
