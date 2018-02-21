c699f9d97b07ccc9a6d9c90a5e0d6d4c65df9d6a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-21 21:58:45 +0100

    add additional test to person isYoungerThan

--- src/test/java/algorithm/PersonTest.java
+++ src/test/java/algorithm/PersonTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static algorithm.SamplePerson.greg;
 import static algorithm.SamplePerson.sue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PersonTest {
@@ -12,4 +13,10 @@ public class PersonTest {
     public void should_be_younger_if_was_born_later() {
         assertTrue(greg.isYoungerThan(sue));
     }
+
+    @Test
+    public void should_not_be_younger_if_was_born_before() {
+        assertFalse(sue.isYoungerThan(greg));
+    }
+
 }
